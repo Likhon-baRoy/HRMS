7b7efb93bbc66bb70845b43af392f19316c06ed8
Likhon Baroi <likhonhere007@proton.me>
2026-05-13 00:06:48 +0600

    JWT Interceptor to automatically attached token to requests

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,28 +1,11 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
-import {
-  provideRouter
-} from '@angular/router';
-
-import {
-  provideHttpClient,
-  withInterceptors
-} from '@angular/common/http';
-
-import {
-  provideAnimations
-} from '@angular/platform-browser/animations';
-
-import {
-  routes
-} from './app.routes';
-
-export const appConfig:
-  ApplicationConfig =
+export const appConfig: ApplicationConfig =
 {
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -32,7 +15,11 @@ export const appConfig:
 
     provideRouter(routes),
 
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor
+      ])
+    ),
 
     provideAnimations()
   ]
--- client/src/app/features/dashboard/dashboard/dashboard.ts
+++ client/src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,29 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+
 import { AuthService } from '../../../core/services/auth.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-dashboard',
-
   standalone: true,
-
   imports: [
     MatToolbarModule,
     MatButtonModule
   ],
-
   templateUrl: './dashboard.html',
-
-  styleUrl: './dashboard.scss',
+  styleUrl: './dashboard.scss'
 })
-
-export class Dashboard
-{
+export class Dashboard implements OnInit {
   private auth = inject(AuthService);
+  private http = inject(HttpClient);
 
-  logout(): void
-  {
+  stats: any = null;
+
+  ngOnInit(): void {
+    this.loadDashboard();
+  }
+
+  loadDashboard(): void {
+    this.http
+      .get(`${environment.apiUrl}/dashboard`)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.stats = response;
+        },
+
+        error: console.error
+      });
+  }
+
+  logout(): void {
     this.auth.logout();
   }
 }
